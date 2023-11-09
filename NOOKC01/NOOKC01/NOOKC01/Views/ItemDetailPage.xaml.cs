25c2ac1dc45edf667b1f189f76598b07374bf05f
nookc01 <chivisailam01@gmail.com>
2023-11-09 17:36:34 +0100

    first commit

---
+++ NOOKC01/NOOKC01/NOOKC01/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using NOOKC01.ViewModels;
+
+namespace NOOKC01.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
